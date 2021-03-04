365192fe02b63b0db28961338760fe9e76f0a09e
Antoine161 <castellanos.antonio1.61@gmail.com>
2021-03-03 19:27:38 -0600

    dibujé una distribución exponencial

--- prueba.py
+++ prueba.py
@@ -8,3 +8,11 @@ Created on Wed Mar  3 19:05:20 2021
 #esto es una prueba. 
 
 print("Hola vatos")
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#vamos a graficar una distribución exponencial
+x = np.random.exponential(0.5,100**3)
+plt.hist(x)
+
